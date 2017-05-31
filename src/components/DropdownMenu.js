607f1e95a48ea7e53a2b276748f76f9f2181e694
Matt Paul <mattpaul001@gmail.com>
2017-05-31 19:42:37 +0100

    SVGs imported, html for menu sketched out

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -1,7 +1,45 @@
 import React from 'react';
+import iconCreate from '../visuals/icon-create.svg';
+import iconSettings from '../visuals/icon-settings.svg';
+import iconYourCourses from '../visuals/icon-your-courses.svg';
+import iconYourProfile from '../visuals/icon-your-profile.svg';
+import iconSignOut from '../visuals/icon-sign-out.svg';
 
 export default function DropdownMenu() {
   return (
-    <h3>I'm the dropdown</h3>
+    <div>
+      <ul>
+        <li>
+          <a href="#">
+            <img src={iconYourCourses} alt="your-courses-icon" />
+            <span>Your courses</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img src={iconCreate} alt="create-icon" />
+            <span>Create</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img src={iconYourProfile} alt="your-profile-icon" />
+            <span>Your profile</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img src={iconSettings} alt="settings-icon" />
+            <span>Settings</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img src={iconSignOut} alt="sign-out-icon" />
+            <span>Sign out</span>
+          </a>
+        </li>
+      </ul>
+    </div>
   )
 }
